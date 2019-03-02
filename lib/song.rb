eedec170d1ef9cb4e3e7d640373ea881122d2d41
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-03-02 00:27:39 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0
+  @@genre_count = {}
+  @@artist_count = {}
+  
+  def initialize(name, artist, genre)
+    @@count += 1 
+    @@genres = [genre]
+    @@artists = [artist]
+    @@genre_count
+    @@artist_count
+  end 
+  
+  def self.count 
+    @@song_count
+  end 
+  
+  def self.genres
+    @@genres = []
+    @@genres << @genre.each {|genre|}
+  end
+  
+  def self.artist
+    @@artists = [] 
+  end 
+  
+  def self.genre_count
+    @@genre_count = {}
+  end 
+  
+  def self.artist_count 
+    @@artist_count = {}
+  end 
+  
+  
+end 
